3ebc026f95ffda1733088d85e8123149bee270fb
Towhid-Raiyan <towhid.raiyan.com>
2023-05-20 02:18:35 +0600

    navbar added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
+            }
+            // {
+            //     path:'/addtoy',
+            //     element:<AddToy/>
+            // },
+            // {
+            //     path:'alltoys',
+            //     element:<AllToys/>
+            // }
+        ],
+    },
+]);
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+// import AuthProvider from './Provider/AuthProvider.jsx';
+import router from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      {/* <AuthProvider> */}
+      {/* {" "} */}
+      <RouterProvider router={router}></RouterProvider>
+      {/* </AuthProvider> */}
+    </React.StrictMode>
+  </div>
+);
