311f2fa9f8dfde601662d32e717a67a0f324711b
Towhid-Raiyan <towhid.raiyan.com>
2023-05-21 05:22:43 +0600

    sql and nosql comparison added in blogs

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -8,6 +8,11 @@ const Blogs = () => {
                 <p>
                     An access token is a credential granted to a client application after successful authentication, allowing it to access specific resources or perform actions on behalf of a user. It is short-lived and has an expiration time. A refresh token is also issued alongside the access token and has a longer lifespan. When the access token expires, the refresh token can be used to obtain a new access token without requiring the user to reauthenticate. Access tokens can be stored in memory or secure storage mechanisms like sessionStorage, while refresh tokens should be stored in HTTP-only secure cookies or secure storage mechanisms like localStorage, following security best practices such as encryption and HTTPS communication.</p>
             </div>
+            <div className="p-4 my-4 border border-cyan-400 rounded-lg bg-cyan-100">
+                <h3 className="font-bold">Compare SQL and NoSQL databases?</h3>
+                <p>
+                    SQL and NoSQL databases differ in their data models, scalability, query language, schema flexibility, and use cases. SQL databases have a structured schema, use SQL as a query language, and are vertically scalable. They are ideal for complex querying, transactions, and relational data. NoSQL databases have flexible schemas, use various data models, and are horizontally scalable. They excel in handling large amounts of data, high write loads, and unstructured or evolving data. Use NoSQL for web applications, real-time data, and scenarios where flexibility is essential.</p>
+            </div>
             
         </div>
     );
